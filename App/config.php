bbb500a68e3850774abfb1651f568f9c51f7a74e
Evanzyker <quentin.rimoux@live.fr>
2016-04-06 21:49:22 +0200

    Initial commit - Encore rien de modifié à part les tables sql.

---
+++ App/config.php
@@ -0,0 +1,9 @@
+<?php
+define("hostname","localhost"); // ou serveurmysql
+
+define("database","S3projet2");
+define("username","votreLogin");
+define("password","votreMotDePasse");
+
+define('BASE_URL', explode('app_dev.php', $_SERVER['SCRIPT_NAME'])[0]);
+//define("BASE_URL","http://votreURL/");
